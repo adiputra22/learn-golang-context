1ddd0396c952e26ea8bce0aa01b0983fa95e734e
Adiputra <tuts.adiputra@gmail.com>
2021-09-28 07:38:34 +0700

    initial context child parent

---
+++ context_test.go
@@ -0,0 +1,40 @@
+package belajar_golang_context
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	backgroud := context.Background()
+	fmt.Println(backgroud)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "f")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+
+	fmt.Println(contextA.Value("b"))
+}
